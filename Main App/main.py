700e8c67c7716f83b9722626a29abff5705c0e6d
vikas32210 <vikas.yadav.322005@gmail.com>
2026-09-03 02:10:26 +0530

    Add STUN servers to RTC configuration

--- Main App/main.py
+++ Main App/main.py
@@ -564,8 +564,17 @@ def main():
         # --------------------------------------------------------
 
         rtc_configuration = {
-            "iceServers": []
+    "iceServers": [
+        {
+            "urls": [
+                "stun:stun.l.google.com:19302",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun3.l.google.com:19302"
+            ]
         }
+    ]
+}
 
         # --------------------------------------------------------
         # START WEBRTC
