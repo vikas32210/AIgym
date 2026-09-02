5323eed02623a63631341f81af83a1ca9ce45d97
vikas32210 <vikas.yadav.322005@gmail.com>
2026-09-03 02:08:46 +0530

    Update main.py

--- Main App/main.py
+++ Main App/main.py
@@ -564,16 +564,7 @@ def main():
         # --------------------------------------------------------
 
         rtc_configuration = {
-            "iceServers": [
-                {
-                    "urls": [
-                        "stun:stun.l.google.com:19302",
-                        "stun:stun1.l.google.com:19302",
-                        "stun:stun2.l.google.com:19302",
-                        "stun:stun3.l.google.com:19302",
-                    ]
-                }
-            ]
+            "iceServers": []
         }
 
         # --------------------------------------------------------
